4b8e06d032753da44e8c366ae01df698c9562de6
sbasu1 <ghoshsreya@gmail.com>
2020-02-16 22:51:26 -0600

    Query added

--- queries.sql
+++ queries.sql
@@ -1,7 +1,8 @@
 ## Part 1: Test it with SQL
-
+select * from job
 ## Part 2: Test it with SQL
-
+select * from employer where location='St. Louis'
 ## Part 3: Test it with SQL
-
+DROP TABLE job
 ## Part 4: Test it with SQL
+SELECT s.name,s.description FROM skill s where s.id in (select skills_id from job_skills) order by name
